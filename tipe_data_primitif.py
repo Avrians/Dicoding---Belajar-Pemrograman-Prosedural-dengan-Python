843af0621dc403095352aa9183ec509d5d252f5f
avrians <avriansyahb10@gmail.com>
2024-03-11 09:37:52 +0700

    fist commit

---
+++ tipe_data_primitif.py
@@ -0,0 +1,24 @@
+# Boolean 
+hasTerrace = True
+hasGarage = False
+
+# Numerik
+numberOfRestroom = 3
+heightOfRestroom = 2
+widthOfRestroom = 1.5
+
+# String
+buildingArea = "88 meter persegi"
+homeAddress = 'Jl. Batik Kumeli No. 50, Bandung'
+facilities = "Pasar tradisional, tempat olahraga, SPBU"
+
+# Mengecek tipe data
+a = 5
+b = "Hello"
+c = 4.5
+d = False
+ 
+print(type(a))  # <class 'int'>
+print(type(b))  # <class 'str'>
+print(type(c))  # <class 'float'>
+print(type(d))  # <class 'bool'>
