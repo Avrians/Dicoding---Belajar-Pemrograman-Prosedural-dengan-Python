d6fb1c2c1e72f81df55bc50e18d7cba911a3e4d9
avrians <avriansyahb10@gmail.com>
2024-03-13 17:29:51 +0700

    [av][update][add soal]

--- Quis/01_format_jam.py
+++ Quis/01_format_jam.py
@@ -1,3 +1,12 @@
+# Soal
+"""
+Bapak Fajar sedang bekerja di tempat proyek pembangunan rumah. Seluruh karyawan melakukan presensi secara mandiri melalui check clock yang tersedia di proyek tersebut, termasuk Bapak Fajar. Hal ini diwajibkan untuk mengetahui waktu kedatangan dan kepulangan setiap karyawan.
+
+Bapak Fajar datang pada pukul 09:23:57 dan pulang pada pukul 15:25:43. Selama lebih dari 6 jam, beliau bekerja semaksimal mungkin untuk menyelesaikan tugas pada hari tersebut. Sistem check clock akan mencatat waktu kedatangan dan kepulangan Bapak Fajar secara otomatis.
+
+Untuk mempermudah proses pencatatan, buatlah program sederhana yang mampu menyimpan waktu kedatangan dan kepulangan ke dalam variabel bertipe data dictionary.
+"""
+
 """
 TODO:
  1. Buatlah variabel startTime bertipe data dictionary sebagai waktu datang dengan kriteria berikut.
